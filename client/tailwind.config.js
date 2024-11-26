a381412436496b46a052917f8edcbe8fabb87a35
Minte <mintesinottamene0917@gmail.com>
2024-11-26 18:30:05 +0300

    npm install flowbite-react & config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite/plugin");
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}", // Add this line to include Flowbite's content
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite, // Use the Flowbite plugin directly
+  ],
 };
