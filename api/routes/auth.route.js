3f9f5e3d9450544540442180b697a3af88813066
Minte <mintesinottamene0917@gmail.com>
2024-12-01 16:45:07 +0300

    Merge branch 'server-side' into Front-End

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js"; // Ensure both are imported
 
 const router = express.Router();
 
+// Define routes
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
